ad01f36c0b46fb2bbf66a87fa58141b6746e036c
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-03-05 14:15:15 -0800

    Commit
    Created the PastOrderViewController.swift file.

---
+++ Bot Shop/PastOrderViewController.swift
@@ -0,0 +1,12 @@
+import Foundation
+import UIKit
+
+class PastOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+}
