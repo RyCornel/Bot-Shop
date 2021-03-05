9898cbd62490d9883b8ed7be14b9f32f1cabbe36
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-03-05 14:44:08 -0800

    Commit
    Created extension for NewOrderViewController.swift file.

--- Bot Shop/PastOrderViewController.swift
+++ Bot Shop/PastOrderViewController.swift
@@ -6,7 +6,7 @@ class PastOrderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .green
+        view.backgroundColor = .green 
     }
 
 }
