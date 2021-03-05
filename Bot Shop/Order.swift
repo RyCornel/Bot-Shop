cbc532bf7b281bfab928f6ad9b119d6f20f62b06
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-03-05 15:22:09 -0800

    Commit
    Created Order.swift file.

---
+++ Bot Shop/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  Bot Shop
+//
+//  Created by Ryan Cornel on 3/5/21.
+//
+
+import Foundation
+import UIKit
+
+struct Order {
+    var title: String
+    var image: UIImage
+}
--- Bot Shop/PastOrderViewController.swift
+++ Bot Shop/PastOrderViewController.swift
@@ -38,5 +38,8 @@ extension PastOrderViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
 
 }
