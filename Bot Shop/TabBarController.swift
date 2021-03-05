9eae18fda5136932772ded49d69b974689324b0a
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-03-05 14:22:42 -0800

    Commit
    Corrected mistake. Placed functions outside of viewDidLoad().

--- Bot Shop/TabBarController.swift
+++ Bot Shop/TabBarController.swift
@@ -10,17 +10,37 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate{
         super.viewDidLoad()
         self.delegate = self
         
-        func setupTabBarIcons(){
-
-            let firstItemView = self.tabBar.subviews[0]
-            let secondItemView = self.tabBar.subviews[1]
-
-            self.firstItemImageView = (firstItemView.subviews.first as! UIImageView)
-            self.firstItemImageView.contentMode = .center
-
-            self.secondItemImageView = (secondItemView.subviews.first as! UIImageView)
-            self.secondItemImageView.contentMode = .center
-        }
+        setupViewControllers()
+        setupTabBarIcons()
+        
     }
+    
+    func setupTabBarIcons(){
+
+        let firstItemView = self.tabBar.subviews[0]
+        let secondItemView = self.tabBar.subviews[1]
+
+        self.firstItemImageView = (firstItemView.subviews.first as! UIImageView)
+        self.firstItemImageView.contentMode = .center
+
+        self.secondItemImageView = (secondItemView.subviews.first as! UIImageView)
+        self.secondItemImageView.contentMode = .center
+    }
+    
+    func setupViewControllers(){
+
+        let newBoxVC = NewOrderViewController()
+        let navController1 = UINavigationController(rootViewController:newBoxVC)
+        newBoxVC.tabBarItem = UITabBarItem(title: "New", image: UIImage(named: "tab-box"), tag: 0)
+
+        let pastBoxesVC = PastOrderViewController()
+        let navController2 = UINavigationController(rootViewController:pastBoxesVC)
+        pastBoxesVC.tabBarItem = UITabBarItem(title: "History", image: UIImage(named: "tab-history"), tag: 1)
+
+
+        viewControllers = [navController1, navController2]
+
+    }
+
 
 }
