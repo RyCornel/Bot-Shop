a43946a782a463f1f38e1114e460b79c71d5388b
Ryan Cornel <ry.cornel@students.makeschool.com>
2021-03-05 14:19:54 -0800

    Commit
    Updated TabBarController.swift with setupTabBarIcons() function.

--- Bot Shop/TabBarController.swift
+++ Bot Shop/TabBarController.swift
@@ -2,10 +2,25 @@ import Foundation
 import UIKit
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate{
-
+    
+    var firstItemImageView: UIImageView!
+    var secondItemImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
+        
+        func setupTabBarIcons(){
+
+            let firstItemView = self.tabBar.subviews[0]
+            let secondItemView = self.tabBar.subviews[1]
+
+            self.firstItemImageView = (firstItemView.subviews.first as! UIImageView)
+            self.firstItemImageView.contentMode = .center
+
+            self.secondItemImageView = (secondItemView.subviews.first as! UIImageView)
+            self.secondItemImageView.contentMode = .center
+        }
     }
 
 }
